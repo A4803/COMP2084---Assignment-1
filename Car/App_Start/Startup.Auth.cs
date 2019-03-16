2066ce8eb2e86ea4230895cee96bf56dd5b98c18
s2d130050131049 <alaynavagamia96@gmail.com>
2019-03-16 13:56:25 -0400

    Signin with google

--- Car/App_Start/Startup.Auth.cs
+++ Car/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Car.Models;
+using System.Configuration;
 
 namespace Car
 {
@@ -58,11 +59,11 @@ namespace Car
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["GoogleClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["GoogleClientSecret"]
+            });
         }
     }
 }
